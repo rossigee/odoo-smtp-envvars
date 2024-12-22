1fd286ce7d7d72c132afabc2e4fb329e57e44f7d
Ross Golder <ross@golder.org>
2024-12-22 11:40:14 +0700

    Bump module version.

--- mail_smtp_envvars/__manifest__.py
+++ mail_smtp_envvars/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Get SMTP settings from environment variables',
-    'version': '15.0.1.0.1',
+    'version': '17.0.1.0.1',
     'author': 'Ross Golder',
     'website': 'https://golder.org/',
     'license': 'AGPL-3',
